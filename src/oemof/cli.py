64d8c1957eb458067304a5208f4604291473b25e
uvchik <uvchik.git@posteo.eu>
2020-04-02 17:18:24 +0200

    Fix long line

--- src/oemof/cli.py
+++ src/oemof/cli.py
@@ -3,8 +3,8 @@ Module that contains the command line app.
 
 Why does this file exist, and why not put this in __main__?
 
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
+  You might be tempted to import things from __main__ later, but that will
+  cause problems: the code will get executed twice:
 
   - When you run `python -moemof` python will execute
     ``__main__.py`` as a script. That means there won't be any
